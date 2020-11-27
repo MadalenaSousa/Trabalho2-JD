67e179a9c7fe5bfecd41c5d327d6de3408914b6f
Madalena Sousa <madalenaantunessousa@gmail.com>
2020-11-27 12:43:23 +0000

    Basic Player Movement

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody2D body;
+    public float moveInputX, moveInputY;
+    public float speed;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        moveInputX = Input.GetAxis("Horizontal");
+        moveInputY = Input.GetAxis("Vertical");
+        body.velocity = new Vector2(moveInputX * speed, moveInputY * speed);
+    }
+}
