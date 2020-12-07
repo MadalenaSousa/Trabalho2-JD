efb929681e04ab43388002f49a5140e5c5d52198
Madalena Sousa <madalenaantunessousa@gmail.com>
2020-12-07 16:43:15 +0000

    Enigma Panel Test

---
+++ Assets/Scripts/Sphinx1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphinx1 : MonoBehaviour
+{
+    public GameObject firstEnigmaPanel;
+
+    void Start()
+    {
+        firstEnigmaPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            firstEnigmaPanel.SetActive(true);
+        }
+    }
+
+    public void closePanel()
+    {
+        firstEnigmaPanel.SetActive(false);
+        Destroy(GameObject.FindGameObjectWithTag("Door1")); //This is not here, this is in a function triggered by solving the first enigma, this is only for demonstrantion purpuses
+    }
+}
