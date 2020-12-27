6aecf5243be35382bd30ed277bb09fe9f62b8939
Madalena Sousa <madalenaantunessousa@gmail.com>
2020-12-27 17:03:37 +0000

    Inventory List of Items (Part 1)

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogWarning("More than one Inventory!");
+        }
+
+        instance = this;
+    }
+
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+
+    public List<Item> items = new List<Item>();
+
+    public void AddItem(Item item)
+    {
+        items.Add(item);
+        
+        if(onItemChangedCallback != null)
+        {
+            onItemChangedCallback.Invoke();
+        }
+    }
+
+    public void RemoveItem(Item item)
+    {
+        items.Remove(item);
+        if (onItemChangedCallback != null)
+        {
+            onItemChangedCallback.Invoke();
+        }
+    }
+}
