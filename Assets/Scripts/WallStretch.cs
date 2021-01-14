b9c4b025294122422c69064219efb76f55a649cb
Madalena Sousa <madalenaantunessousa@gmail.com>
2021-01-14 14:58:45 +0000

    Squashing Walls

--- Assets/Scripts/WallStretch.cs
+++ Assets/Scripts/WallStretch.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class WallStretch : MonoBehaviour
 {
-    
+    SpriteRenderer wallSprite;
+    float maxWallWidth, minWallWidth;
+    float newWidth;
+    public float inc = 1;
+    float healthValue = 1;
+
     void Start()
     {
-        Debug.Log(GetComponent<SpriteRenderer>().drawMode);
+        wallSprite = GetComponent<SpriteRenderer>();
+        wallSprite.drawMode = SpriteDrawMode.Sliced;
+        maxWallWidth = wallSprite.size.x;
+        minWallWidth = 2;
+        newWidth = minWallWidth;
     }
 
     
     void Update()
     {
-        
+        if(wallSprite.size.x >= maxWallWidth)
+        {
+            inc = - Mathf.Abs(inc);
+        }
+        else if(wallSprite.size.x <= minWallWidth)
+        {
+            inc = Mathf.Abs(inc);
+        }
+
+        newWidth = newWidth + inc;
+        wallSprite.size = new Vector2(newWidth, wallSprite.size.y);
+    }
+
+    private void OnCollisionStay2D(Collision2D info)
+    {
+        if (info.gameObject.tag == "Player")
+        {
+            PlayerControl.instance.currentPlayer.decreaseHealth(healthValue);
+        }
     }
 }
