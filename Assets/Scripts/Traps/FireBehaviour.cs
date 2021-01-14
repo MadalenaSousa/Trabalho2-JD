72ab7f5babe4b639e5c687aaf783b735fd7f2b10
Madalena Sousa <madalenaantunessousa@gmail.com>
2021-01-14 13:45:57 +0000

    Fire Traps

---
+++ Assets/Scripts/Traps/FireBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBehaviour : MonoBehaviour
+{
+    public float fireInterval = 1f;
+    public float startTime = 0;
+    GameObject fire;
+    bool switchState;
+
+    void Start()
+    {
+        fire = gameObject.transform.GetChild(0).gameObject;
+        fire.SetActive(false);
+        switchState = false;
+    }
+
+    
+    void Update()
+    {
+        if (Time.time > startTime)
+        {
+            startTime = Time.time + fireInterval;
+
+            switchState = !switchState;
+        }
+
+        if(switchState)
+        {
+            fire.SetActive(true);
+        } 
+        else
+        {
+            fire.SetActive(false);
+        }
+    }
+}
+
---
+++ Assets/Scripts/WallStretch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallStretch : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Debug.Log(GetComponent<SpriteRenderer>().drawMode);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
