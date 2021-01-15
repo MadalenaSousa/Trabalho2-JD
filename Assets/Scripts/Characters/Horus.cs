1f78f3192a89dd9ca0b564f8ba28cf9a7e692458
Madalena Sousa <madalenaantunessousa@gmail.com>
2021-01-15 15:00:00 +0000

    Deactivate buttons after dying

--- Assets/Scripts/Characters/Horus.cs
+++ Assets/Scripts/Characters/Horus.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.Linq;
 
 public class Horus : Player
 {
+    public Button horusButton;
+
     public Horus()
     {
         playerName = "Horus";
@@ -25,10 +28,12 @@ public class Horus : Player
         if (isDead)
         {
             GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 1);
+            horusButton.interactable = false;
         }
         else
         {
             GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+            horusButton.interactable = true;
         }
     }
 }
