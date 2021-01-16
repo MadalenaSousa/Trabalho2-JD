48a36b442c82a063e6bbb6116e7f0e26ea61ff2a
Madalena Sousa <madalenaantunessousa@gmail.com>
2021-01-16 17:30:09 +0000

    I don't even know anymore... i need to go study TCM

--- Assets/Scripts/OpenNextLevelDoor.cs
+++ Assets/Scripts/OpenNextLevelDoor.cs
@@ -5,15 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class OpenNextLevelDoor : MonoBehaviour
 {
+    public Sprite doorOpen;
+    public GameObject movingCamera;
+
     private void OnTriggerEnter2D(Collider2D info)
     {
         if (info.gameObject.tag == "Player" && GameManager.instance.hasAllAnswers)
         {
-            SceneManager.LoadScene("Win");
+            gameObject.GetComponent<SpriteRenderer>().sprite = doorOpen;
+            StartCoroutine(ExecuteAfterTime(3));
         }
         else
         {
             Debug.Log("You need to solve all riddles first!");
         }
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        SceneManager.LoadScene("Win");
     }
 }
