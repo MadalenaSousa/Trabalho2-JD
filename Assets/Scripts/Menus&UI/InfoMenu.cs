812a69ac659047e2329d5d628e2bdd6eaadbcfe5
Madalena Sousa <madalenaantunessousa@gmail.com>
2021-01-24 16:48:46 +0000

    Renaming Info Menu Script

--- Assets/Scripts/Menus&UI/InfoMenu.cs
+++ Assets/Scripts/Menus&UI/InfoMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SwitchTabs : MonoBehaviour
+public class InfoMenu : MonoBehaviour
 {
     public GameObject inventoryPanel, characterInfoPanel, infoMenu;
     public Sprite chestClosed;
